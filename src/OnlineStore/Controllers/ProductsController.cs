57480eb105a8a96e678b8b77f79c64377f097d50
Cassie Musolf <cassiemusolf@gmail.com>
2017-05-16 10:10:45 -0700

    add dropdown for the category and get it pushed to the database correctly.

--- src/OnlineStore/Controllers/ProductsController.cs
+++ src/OnlineStore/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnlineStore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace OnlineStore.Controllers
 {
