1ed7da0bd0a9a4108eaa7d1e3caa7ba318a88da3
Cassie Musolf <cassiemusolf@gmail.com>
2017-05-16 13:16:48 -0700

    add shopping cart method to add product to shopping cart.

---
+++ src/OnlineStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OnlineStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void AddToCart(Product product)
+        {
+
+            var cartItem = db.ShoppingCarts.SingleOrDefault(c => c.ProductId == product.Id);
+
+            if (cartItem == null)
+            {
+                cartItem = new ShoppingCart
+                {
+                    ProductId = product.Id,
+                    ProductCount = 1,
+                    Today = DateTime.Now
+                };
+            
+                db.ShoppingCarts.Add(cartItem);
+            }
+
+            else
+            {
+                cartItem.ProductCount++;
+            }
+            db.SaveChanges();
+        }
+    }
+}
